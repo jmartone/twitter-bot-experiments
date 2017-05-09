9d71d23a2c7640f8c7d38e23fd8c970a8f9bc97f
Josh Martone <j.martone@bigspaceship.com>
2017-05-09 17:33:07 -0400

    It actually works now

--- TwitterBot.py
+++ TwitterBot.py
@@ -2,59 +2,70 @@ import tweepy
 from queue import Queue
 import threading
 
+def threaded(fn):
+	def wrapper(*args, **kwargs):
+		thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
+		thread.setDaemon(True)
+		thread.start()
+		return thread
+	return wrapper
+
 class TwitterBot(object):
 
-	def threaded(fn):
-	    def wrapper(*args, **kwargs):
-	        thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
-	        thread.setDaemon(True)
-	        thread.start()
-	        return thread
-	    return wrapper
-
-	def __init__(self, CREDS):
+	def __init__(self, CREDS, filters, callback):
 		# intialize twitter API
 		self.twitterAuth = tweepy.OAuthHandler(CREDS['CONSUMER_KEY'], CREDS['CONSUMER_SECRET'])
 		self.twitterAuth.set_access_token(CREDS['ACCESS_TOKEN'], CREDS['ACCESS_TOKEN_SECRET'])
+		# set the filters
+		if (type(filters) == list):
+			self.filters = filters
+		else:
+			self.filters = [filters]
+		self.callback = callback
 
-	@threaded
-	def handleQueue():
-	    while True:
-	        status = q.get()
-	        self.handleStatus(status)
-	        q.task_done()
+
 
 	@threaded
 	def _startThreaded(self):
 		self._start()
 
-    def _start(self):
-
-    	# set up listener class
+	def _start(self):
+		# set up listener class
 		class BotListener(tweepy.StreamListener):
-	    	def on_status(self, status):
-	        	q.put(status)
 
-		    def on_error(self, status_code):
-		        if status_code == 420:
-		            print("Rate limited!")
-		            return False
+			def __init__(self, callback, *args, **kwargs):
+				self.q = Queue()
+				self.handleQueue()
+				self.callback = callback
+				tweepy.StreamListener.__init__(self)
 
-     	# TODO: HANDLE DISCONNECT ERRORS
+			@threaded
+			def handleQueue(self):
+				while True:
+					status = self.q.get()
+					self.callback(status)
+					self.q.task_done()
+
+			def on_status(self, status):
+				self.q.put(status)
+
+			def on_error(self, status_code):
+				if status_code == 420:
+					print("Rate limited!")
+					return False
+
+		# TODO: HANDLE DISCONNECT ERRORS
 		while True:
-		    try:
-		        listener = BotListener()
-		        stream = tweepy.Stream(auth = twitterAuth, listener=listener)
-		        stream.filter(track = filters)
-		    except KeyboardInterrupt:
-		        # Or however you want to exit this loop
-		        stream.disconnect()
-		        break
+			try:
+				listener = BotListener(self.callback)
+				stream = tweepy.Stream(auth = self.twitterAuth, listener = listener)
+				stream.filter(track = self.filters)
+			except KeyboardInterrupt:
+				# Or however you want to exit this loop
+				stream.disconnect()
+				break
 	
-	def start(self, filters, handleStatus, threaded = False):
-		self.handleStatus = handleStatus
-		q = Queue()
-		self.handleQueue()
+	def start(self, threaded = False):
 		if (threaded):
 			self._startThreaded()
 		else:
